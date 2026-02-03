40cbb62314f306263e11de2ab6acd804fec6adaf
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-02-02 22:46:10 -0300

    feat(auth-layout): add forms container

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component:
-    },
-    {
-        path: 'dashboard',
-        component:
-        canActivate: [AuthGuard]
-    },
+    // {
+    //     path: 'login',
+    //     component:
+    // },
+    // {
+    //     path: 'dashboard',
+    //     component:
+    //     canActivate: [AuthGuard]
+    // },
     {
         path: '',
         redirectTo: 'login',
