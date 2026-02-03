4214cd0b44be321691b7368351d50ed85ca06805
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-02-03 20:50:47 -0300

    feat(login): create component

---
+++ client/src/app/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormInput } from '../shared/ui/form-input/form-input';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormInput],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+
+}
