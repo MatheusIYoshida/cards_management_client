a043df4d67d9410411e4054e449a07c515b8e9a7
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-02-03 20:51:18 -0300

    feat(form-input): create component

---
+++ client/src/app/shared/ui/form-input/form-input.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-form-input',
+  imports: [],
+  templateUrl: './form-input.html',
+  styleUrl: './form-input.scss',
+})
+export class FormInput {
+  @Input() enableLabel!: boolean;
+  @Input() labelText!: string;
+  @Input() maxLength!: number;
+}
