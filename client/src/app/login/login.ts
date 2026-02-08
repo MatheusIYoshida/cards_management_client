4780b2381acb6ee236d15b1dc06fc3b595c4fca0
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-02-08 20:14:19 -0300

    fix (login): change href to routerLink

--- client/src/app/login/login.ts
+++ client/src/app/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FormInput } from '../shared/ui/form-input/form-input';
 import { FormButton } from '../shared/ui/form-button/form-button';
 
 @Component({
   selector: 'app-login',
-  imports: [FormInput, FormButton],
+  imports: [FormInput, FormButton, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
