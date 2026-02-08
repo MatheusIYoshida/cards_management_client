93d9d5928bb2b17bec076aa708c8e89c8db2c752
Matheus Yoshida <yoshidaimatheus@gmail.com>
2026-02-08 20:35:47 -0300

    feat (register): base component completed

--- client/src/app/register/register.ts
+++ client/src/app/register/register.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FormInput } from '../shared/ui/form-input/form-input';
 import { FormButton } from '../shared/ui/form-button/form-button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [FormInput, FormButton],
+  imports: [FormInput, FormButton, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
-
+  signinText: string = 'Already have an account?';
+  signinLinkText: string = 'Sign in';
 }
